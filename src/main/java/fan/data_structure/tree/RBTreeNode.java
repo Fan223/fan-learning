d9fb170e4d9557ff15891212eeca63800799f46e
Fan223 <fanj.zh@foxmail.com>
2023-05-26 17:32:30 +0800

    update

--- src/main/java/fan/data_structure/tree/RBTreeNode.java
+++ src/main/java/fan/data_structure/tree/RBTreeNode.java
@@ -1,14 +1,28 @@
-package fan.data_structure.tree;//package fan.tree;
-//
-//public class RBTreeNode<T extends Comparable<T>> {
-//    boolean color; // red or black
-//    T key; // 键
-//    RBTreeNode leftChildren; // 左子节点
-//    RBTreeNode rightChildren; // 右子节点
-//    RBTreeNode parent; // 父节点
-//
-//    public RBTreeNode(T key, boolean color) {
-//        this.key = key;
-//        this.color = color;
-//    }
-//}
+package fan.data_structure.tree;
+
+/**
+ * 红黑树节点
+ *
+ * @author Fan
+ * @since 2023/5/23 9:42
+ */
+public class RBTreeNode<T extends Comparable<T>> {
+
+    /**
+     * red or black
+     */
+    boolean color;
+
+    T key;
+
+    RBTreeNode leftChildren;
+
+    RBTreeNode rightChildren;
+
+    RBTreeNode parent;
+
+    public RBTreeNode(T key, boolean color) {
+        this.key = key;
+        this.color = color;
+    }
+}
